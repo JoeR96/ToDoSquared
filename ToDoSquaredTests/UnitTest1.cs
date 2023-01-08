8c94b9aebfd59522f1c0d03de88c16cdef375588
Joe Richardson <joeyrichardson96@gmail.com>
2023-01-08 16:50:39 +0000

    Create Unit Test Project

---
+++ ToDoSquaredTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ToDoSquaredTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Fail();
+    }
+}
